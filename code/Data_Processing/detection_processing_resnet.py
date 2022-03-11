c09b464863a6f7de4c5c30228938607b84d05237
dragosconst <38582034+dragosconst@users.noreply.github.com>
2022-03-11 14:11:24 +0200

    post-processing

---
+++ code/Data_Processing/detection_processing_resnet.py
@@ -0,0 +1,61 @@
+from typing import List, Optional, Dict, Tuple
+
+import torch
+
+from Utils.utils import intersection_over_union as iou
+"""
+There are a couple problems with my ResNet50 network so far. Even the one that used on-line augmentation has a LOT of
+false positives, but, fortunately enough, they almost always overlap true positives and almost always have a lower score
+than a false positive. Therefore, I will apply a second layer of NMS over the results, this time supressing ANY detection
+that overlaps a detection with a greater score, and also suppresses detections with under 0.3 (or perhaps another constant)
+score. The second condition comes in handy with false positives that are not even close to being cards, they very rarely do
+happen, but have almost negligible scores, usually.
+"""
+
+# in-place operation
+def second_nms(detection: Dict[str, torch.Tensor]) -> None:
+    boxes = detection["boxes"]
+    bad_boxes = set()
+    scores = detection["scores"]
+    labels = detection["labels"]
+    # the boxes are already sorted according to scores
+    print(scores)
+    for idx, box in enumerate(boxes):
+        for box2 in boxes[idx+1:]:
+            if iou(box, box2) >= 0.3:
+                bad_boxes.add(str(box2))
+    good_boxes = []
+    good_scores = []
+    good_labels = []
+    for box, score, label in zip(boxes, scores, labels):
+        if str(box) not in bad_boxes:
+            good_boxes.append(box)
+            good_scores.append(score)
+            good_labels.append(label)
+    detection["boxes"] = torch.stack(good_boxes)
+    detection["scores"] = torch.stack(good_scores)
+    detection["labels"] = torch.stack(good_labels)
+
+
+def filter_under_thresh(detection: Dict[str, torch.Tensor]) -> None:
+    boxes = detection["boxes"]
+    bad_boxes = set()
+    scores = detection["scores"]
+    labels = detection["labels"]
+    # the boxes are already sorted according to scores
+    print(scores)
+    for idx, score in enumerate(scores):
+        if score < 0.25:
+            bad_boxes.add(str(boxes[idx])) # use boxes coordinates because they are the only unique property of bounding boxes
+
+    good_boxes = []
+    good_scores = []
+    good_labels = []
+    for box, score, label in zip(boxes, scores, labels):
+        if str(box) not in bad_boxes:
+            good_boxes.append(box)
+            good_scores.append(score)
+            good_labels.append(label)
+    detection["boxes"] = torch.stack(good_boxes)
+    detection["scores"] = torch.stack(good_scores)
+    detection["labels"] = torch.stack(good_labels)
--- code/Models/Vision/faster.py
+++ code/Models/Vision/faster.py
@@ -27,12 +27,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torchvision.utils import draw_bounding_boxes
 
+
 from Data_Processing.detection_dataset import PlayingCardsFRCNNDataset
 from Utils.utils import intersection_over_union
 from Utils.eval import eval
 from references.engine import train_one_epoch, evaluate
 from Utils.utils import load_dataloader, get_loader
 from Utils.trans import RandomAffineBoxSensitive, RandomPerspectiveBoxSensitive
+from Data_Processing.detection_processing_resnet import second_nms, filter_under_thresh
 
 def train_fccnn_reference(model: torch.nn.Module, optimizer: torch.optim.Optimizer, train_dataloader: torch.utils.data.DataLoader,
                           valid_dataloader: torch.utils.data.DataLoader,
@@ -79,6 +81,16 @@ def train_frcnn(model: torch.nn.Module, optimizer: torch.optim.Optimizer, train_
         with torch.no_grad():
             validate(model, valid_dataloader, device)
 
+def initialize_frcnn(backbone: torch.nn.Module, cls: int, anchor_gen: AnchorGenerator=None, roi_pooler: MultiScaleRoIAlign=None,
+                     image_mean: float=None, image_std: float=None, min_size: int=800,
+                     max_size: int=1333, rpn_head: torch.nn.Module=None) -> torch.nn.Module:
+    # if rpn_head is None:
+    #     rpn_head = RPNHead(backbone.out_channels, anchor_gen.num_anchors_per_location()[0])
+
+    return FasterRCNN(backbone=backbone, num_classes=cls, rpn_anchor_generator=anchor_gen, box_roi_pool=roi_pooler,
+                      image_mean=image_mean, image_std=image_std, min_size=min_size, max_size=max_size,
+                      rpn_head=rpn_head)
+
 @torch.inference_mode()
 def validate(model: torch.nn.Module, valid_dataloader: torch.utils.data.DataLoader, device: str) -> None:
     model.eval()
@@ -112,12 +124,14 @@ def validate(model: torch.nn.Module, valid_dataloader: torch.utils.data.DataLoad
 if __name__ == "__main__":
     # torch.cuda.empty_cache()
     frcnn = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to("cuda")
+    # frcnn = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True).to("cuda")  # low res
     in_features = frcnn.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     frcnn.roi_heads.box_predictor = FastRCNNPredictor(in_features, 55).to("cuda")
-    # backbone = vgg19(pretrained=True)
-    # backbone.out_channels = 512
     # frcnn.backbone = backbone
+
+
+
 
     # TODO: filter out the bad labels from the gigantic dataset
     dataset, dataloader = load_dataloader(batch_size=1)
@@ -125,7 +139,7 @@ if __name__ == "__main__":
     targets = dataset.targets()
     indices = np.asarray([x for x in range(len(dataset))])
     indices = indices[..., np.newaxis]
-    train_idx, _, val_idx, _ = iterative_train_test_split(indices, np.asarray(targets), test_size=0.2)
+    train_idx, _, val_idx, _ = iterative_train_test_split(indices[1:], np.asarray(targets), test_size=0.2)
     # val_set, train_set = torch.utils.data.random_split(dataset, [int(len(dataset) * 1/5), int(len(dataset) * 4/5)])
     # indices = torch.randperm(len(dataset)).tolist()
     random.seed(1)
@@ -134,11 +148,11 @@ if __name__ == "__main__":
     val_set = torch.utils.data.Subset(dataset, val_idx)
     # train_set = torch.utils.data.Subset(dataset, indices[:-4100])
     # val_set = torch.utils.data.Subset(dataset, indices[:-4100])
-    train_set.dataset.transforms = T.MyCompose((RandomAffineBoxSensitive(degrees=(0, 45), prob=0.4),
-                                                RandomPerspectiveBoxSensitive(dist_scale=0.3, prob=0.2)))
+    # train_set.dataset.transforms = T.MyCompose((RandomAffineBoxSensitive(degrees=(0, 45), prob=0.4),
+    #                                             RandomPerspectiveBoxSensitive(dist_scale=0.3, prob=0.2)))
 
-    train_loader = get_loader(train_set, batch_size=1, shuffle=True)
-    valid_loader = get_loader(val_set, batch_size=1, shuffle=False)
+    train_loader = get_loader(train_set, batch_size=12, shuffle=True)
+    valid_loader = get_loader(val_set, batch_size=4, shuffle=False)
 
     params = [p for p in frcnn.parameters() if p.requires_grad]
     sgd = SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
@@ -148,13 +162,43 @@ if __name__ == "__main__":
     # train_frcnn(frcnn, adam, lr_scheduler=lr_sched, train_dataloader=train_loader, valid_dataloader=valid_loader,
     #             device="cuda", num_epochs=30)
 
-    # frcnn.load_state_dict(torch.load("D:\\facultate stuff\\licenta\\data\\frcnn_resnet50_with_aug.pt"))
-    # frcnn.eval()
-    # frcnn.to("cuda")
+    frcnn.load_state_dict(torch.load("D:\\facultate stuff\\licenta\\data\\frcnn_resnet50_with_aug.pt"))
+    frcnn.eval()
+    frcnn.to("cuda")
     # validate(frcnn, valid_loader, "cuda")
 
-    # train_fccnn_reference(frcnn, sgd, lr_scheduler=lr_sched, train_dataloader=train_loader, valid_dataloader=valid_loader,
-                # device="cuda", num_epochs=10)
+    # torch.manual_seed(time.time())
+    # rand_img = torch.randint(high=4199,size=(1,)).item()
+    # print(rand_img)
+    img, targets = dataset[0]
+    img = img.to("cuda")
+    imgs = [img]
+    with torch.inference_mode():
+        print("start pred")
+        pred = frcnn(imgs)
+        print("stop pred")
+        for idx, p in enumerate(pred):
+            filter_under_thresh(p)
+            second_nms(p)
+            print(p)
+            def show(imgs):
+                if not isinstance(imgs, list):
+                    imgs = [imgs]
+                fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+                for i, img in enumerate(imgs):
+                    img = img.detach()
+                    img = F.to_pil_image(img)
+                    axs[0, i].imshow(np.asarray(img))
+                    axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
-    # torch.save(frcnn.state_dict(), "D:\\facultate stuff\\licenta\\data\\frcnn_resnet50_with_aug.pt")
+            drawn_boxes = draw_bounding_boxes(transforms.ConvertImageDtype(torch.uint8)(img.to("cpu")), p["boxes"],
+                                              colors="red")
+            print(len(p["boxes"]))
+            show(drawn_boxes)
+            plt.show()
+            print(targets)
+    # train_fccnn_reference(frcnn, sgd, lr_scheduler=lr_sched, train_dataloader=train_loader, valid_dataloader=valid_loader,
+    #             device="cuda", num_epochs=10)
+
+    # torch.save(frcnn.state_dict(), "D:\\facultate stuff\\licenta\\data\\mobilenet_v3_320_large.pt")
     # torch.save(frcnn.state_dict(), "D:\\facultate stuff\\licenta\\data\\frcnn_custom.pt")
